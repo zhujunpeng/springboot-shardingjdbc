c013133a13dd6cb904c342cce3747e7aef5af1ac
朱君鹏 <zhujunpeng@ijovo.com>
2019-03-16 17:20:57 +0800

    1、去除数据库自动配置 2、开启事务 3、加入配置实体

--- src/main/java/com/zjp/SpringbootMybatisApplication.java
+++ src/main/java/com/zjp/SpringbootMybatisApplication.java
@@ -2,13 +2,20 @@ package com.zjp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tk.mybatis.spring.annotation.MapperScan;
 
 /**
- * 添加缓存支持
+ * 1、去除数据库自动配置
+ * 2、开启事务
+ * 3、加入配置实体
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableTransactionManagement(proxyTargetClass = true)
+@EnableConfigurationProperties
 @MapperScan(basePackages = "com.zjp.dao")
 public class SpringbootMybatisApplication {
 
